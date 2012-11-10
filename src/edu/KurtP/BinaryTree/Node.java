3592d2a1e31fbe6295cc42287ce51cd2bbb49ef1
Kurt Prudhomme <krprudhomme@gmail.com>
2012-11-10 10:58:21 -0600

    First commit

---
+++ src/edu/KurtP/BinaryTree/Node.java
@@ -0,0 +1,70 @@
+package edu.KurtP.BinaryTree;
+
+/**
+ * @author Kurt P
+ * @version 0.3.11102012
+ */
+public class Node {
+    private Node left, right, parent;
+    private int value;
+    
+    public Node(int value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the left
+     */
+    public Node getLeft() {
+        return left;
+    }
+
+    /**
+     * @param left the left to set
+     */
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    /**
+     * @return the right
+     */
+    public Node getRight() {
+        return right;
+    }
+
+    /**
+     * @param right the right to set
+     */
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    /**
+     * @return the parent
+     */
+    public Node getParent() {
+        return parent;
+    }
+
+    /**
+     * @param parent the parent to set
+     */
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
